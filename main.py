90b51cc1088ef73fe32f5d108727ba00c151def5
Ethan <z5532866@ad.unsw.edu.au>
2025-10-04 02:25:59 +1000

    creating repo

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 from dotenv import load_dotenv
-from pydantic import BaseSettings
-from langchain_openai import ChatOpenAI
-from langchain_anthropic import ChatAnthropic
+# from pydantic import BaseSettings
+from langchain_google_genai import ChatGoogleGenerativeAI
 
 load_dotenv()  # Load environment variables from .env file
 
 # Setting up an LLms configuration class
-llm = ChatOpenAI()
-llm2 = ChatAnthropic()
+llm = ChatGoogleGenerativeAI(
+    model="gemini-2.5-pro"
+)
+response = llm.invoke("What is the meaning of life?")
+print(response)
