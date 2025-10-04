86abf0e526a432ef4f5c573cef24f8cd1bc01ce7
Ethan <z5532866@ad.unsw.edu.au>
2025-10-05 00:14:05 +1000

    added agent

--- main.py
+++ main.py
@@ -1,12 +1,50 @@
 from dotenv import load_dotenv
-# from pydantic import BaseSettings
+from pydantic import BaseModel
 from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.output_parsers import PydanticOutputParser
+from langchain.agents import create_tool_calling_agent, AgentExecutor
 
 load_dotenv()  # Load environment variables from .env file
 
+class ResearchResponse(BaseModel):
+    topic: str
+    summary: str
+    sources: list[str]
+    tools_used: list[str]
+
 # Setting up an LLms configuration class
-llm = ChatGoogleGenerativeAI(
-    model="gemini-2.5-pro"
+llm = ChatGoogleGenerativeAI(model="gemini-2.5-pro")
+
+parser = PydanticOutputParser(pydantic_object=ResearchResponse)
+
+prompt = ChatPromptTemplate.from_messages(
+    [
+        (
+            "system",
+            """
+            You are a research assistant that will help generate a research paper.
+            Answer the user query and use neccessary tools. 
+            Wrap the output in this format and provide no other text\n{format_instructions}
+            """,
+        ),
+        ("placeholder", "{chat_history}"),
+        ("human", "{query}"),
+        ("placeholder", "{agent_scratchpad}"),
+    ]
+).partial(format_instructions=parser.get_format_instructions())
+
+agent = create_tool_calling_agent(
+    llm=llm,
+    prompt=prompt,
+    tools=[]
 )
-response = llm.invoke("What is the meaning of life?")
-print(response)
+
+agent_executor = AgentExecutor(agent=agent, tools=[], verbose=True)
+raw_response = agent_executor.invoke({"query": "what is the capital of canada?"})
+print(raw_response)
+
+try:
+    structured_response = parser.parse(raw_response["output"])
+except Exception as e:
+    print("Error parsing response:", e, "Raw response was:", raw_response)
